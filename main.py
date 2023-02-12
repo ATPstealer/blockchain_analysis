f7f751524082ab5e2ac4e7245a6365fa70532e4b
atpstealer <forpubmail@gmail.com>
2023-02-12 12:06:52 +0200

    bc object for all treads

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from include.bc import BC
 import multiprocessing
 import mysql.connector
 
-bc = BC()
-
 
 def handle_tx(tx_id):
+    bc = BC()
     tx = bc.get_transaction(tx_id)
     tx_date = bc.get_tx_date(tx)
     day_uniq_value = 0.0
@@ -31,26 +30,32 @@ if __name__ == '__main__':
     start_block = 774502  # 2023-01-31 23:58:16 last december block
     end_block = 716583  # 2022-01-01 00:13:32 start 2022
 
+    bc = BC()
     pool_obj = multiprocessing.Pool()
 
-    cnx = mysql.connector.connect(user='emailamuli_bc', password='8j$7HRT4GJC7ZB4P', host='77.222.61.40',
-                                  database='emailamuli_bc')
-    cursor = cnx.cursor(buffered=True)
-
-    query = ("SELECT * FROM block WHERE block_height=%s")
+    query = ("SELECT turnover_value FROM block WHERE block_height=%s")
     add_block = ("INSERT INTO block (block_height, time) VALUES (%s, %s)")
     update_block = ("UPDATE block SET day_uniq_value = %s, month_uniq_value = %s, year_uniq_value = %s, turnover_value = %s WHERE block_height = %s")
 
     for block_height in range(start_block, end_block - 1, -1):
-        cursor.execute(query, (str(block_height),))
-        if cursor.rowcount > 0:
-            continue
+        # Check if block have handled
+        try:
+            cnx = mysql.connector.connect(user='emailamuli_bc', password='8j$7HRT4GJC7ZB4P', host='77.222.61.40', database='emailamuli_bc')
+            cursor = cnx.cursor(buffered=True)
+            cursor.execute(query, (str(block_height),))
+            if cursor.rowcount > 0:
+                continue
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
 
         block = bc.get_block(block_height)
         block_date = bc.get_block_date(block)
 
+        # Create null row
         cursor.execute(add_block, (block_height, block_date))
         cnx.commit()
+        cursor.close()
+        cnx.close()
 
         print(str(block_height) + "  Block time: " + str(block_date) + "  Now: " + str(datetime.datetime.now()))
 
@@ -67,9 +72,14 @@ if __name__ == '__main__':
             collect_year_uniq_value += year_uniq_value
             collect_turnover_value += turnover_value
 
-        data_block = (collect_day_uniq_value, collect_month_uniq_value, collect_year_uniq_value, collect_turnover_value, block_height)
-        cursor.execute(update_block, data_block)
-        cnx.commit()
-
-    cursor.close()
-    cnx.close()
+        # Save block data
+        try:
+            cnx = mysql.connector.connect(user='emailamuli_bc', password='8j$7HRT4GJC7ZB4P', host='77.222.61.40', database='emailamuli_bc')
+            cursor = cnx.cursor(buffered=True)
+            data_block = (collect_day_uniq_value, collect_month_uniq_value, collect_year_uniq_value, collect_turnover_value, block_height)
+            cursor.execute(update_block, data_block)
+            cnx.commit()
+            cursor.close()
+            cnx.close()
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
